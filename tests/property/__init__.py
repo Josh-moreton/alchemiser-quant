4466e6999591d7e2f85faf0ffc9bee04a7b7651a
Josh <josh@rwxt.org>
2025-08-13 14:51:55 +0100

    del it all bish

--- tests/property/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Property-based tests for The Alchemiser trading system.
-
-These tests use Hypothesis to generate a wide range of inputs and verify
-that our trading logic holds under all conditions.
-"""
--- tests/utils/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Testing utilities and mock framework for The Alchemiser."""
